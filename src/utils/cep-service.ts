776170856f4da5ea9b2870b82dcc30dc4be6009c
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2023-05-04 15:21:47 -0300

    initial commit

---
+++ src/utils/cep-service.ts
@@ -0,0 +1,10 @@
+/* eslint-disable quotes */
+import { ViaCEPAddress } from '@/protocols';
+import { request } from './request';
+
+async function getAddress(cep: string): Promise<ViaCEPAddress> {
+  const result = await request.get(`https://viacep.com.br/ws/${cep}/json/`);
+  return result.data;
+}
+
+export { getAddress };
